68094112eadaf8644f44a12b6a0a88c54093f629
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-10-21 10:38:49 +0800

    Create qwen-ai-out.js

---
+++ qwen-ai-out.js
@@ -0,0 +1,19 @@
+addEventListener("DOMContentLoaded", () => {
+  const apiUrl = "https://qwen-ai-auth0.lhf.us.kg/" // 这里填写你获得的 worker 地址
+  const outputContainer = document.getElementById("ai-output");
+
+  const postTitle = document.getElementsByClassName("post-title").textContent;
+  const postBeforeContent = document.getElementById("article-container").textContent;
+  const postContent = postBeforeContent.replace(/\n/g, '').replace(/[ ]+/g, ' ').replace(/<pre>[\s\S]*?<\/pre>/g, '').substring(0, 1300);
+
+  const evSource = new EventSource(apiUrl + `/?q=${postTitle},文章内容:${postContent}`);
+  evSource.onmessage = (event) => {
+    if (event.data == "[DONE]") {
+      evSource.close();
+      return;
+    } else {
+        const data = JSON.parse(event.data);
+        outputContainer.innerText += data.response ;
+    }
+  }
+});
