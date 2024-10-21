f1df1bf4a8f13cd8c28b976308ae28c1c5c975bc
liaohengfeng <156047906+liaohengfeng@users.noreply.github.com>
2024-10-21 11:55:19 +0800

    Update qwen-ai-out.js

--- qwen-ai-out.js
+++ qwen-ai-out.js
@@ -1,7 +1,8 @@
 addEventListener("DOMContentLoaded", () => {
-  const apiUrl = "https://qwen-ai-auth0.lhf.us.kg/" // 这里填写你获得的 worker 地址
+  const apiUrl = "https://" //这里填写你的worker地址
   const outputContainer = document.getElementById("ai-output");
 
+  
   const postTitle = document.getElementsByClassName("post-title").textContent;
   const postBeforeContent = document.getElementById("article-container").textContent;
   const postContent = postBeforeContent.replace(/\n/g, '').replace(/[ ]+/g, ' ').replace(/<pre>[\s\S]*?<\/pre>/g, '').substring(0, 1300);
